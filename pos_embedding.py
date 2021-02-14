8960daf73354b3b834ac90667263c1158b7b5ad1
edmondja <edmond@jacoupeau.com>
2021-02-14 18:25:06 +0100

    Code

---
+++ pos_embedding.py
@@ -0,0 +1,22 @@
+import numpy as np
+import keras.backend as K
+
+def pos_embedding(x):
+    ''' Embedding layer as in Attention Is All You Need.
+        Can be used this way :
+        Lambda(lambda x: pos_embedding(x) + x)(embedding_layer) '''
+    
+    dmodel = K.cast(K.shape(x)[-1], K.dtype(x))
+    pos = K.cumsum(x * 0 + 1, axis=-2)
+    i = K.cumsum(x * 0 + 1, axis=-1)
+    even_mask = i % 2
+    
+    pos_emb = pos / K.pow(K.cast(10000, K.dtype(x)), 2 * i / dmodel)
+    pos_emb = even_mask * K.sin(pos_emb) + (1 - even_mask) * K.cos(pos_emb)
+    
+    return pos_emb
+
+tensor = np.random.uniform(size=40).reshape((10, 4))
+tensor = K.variable(value=tensor, dtype='float32', name='example')
+embeddings = pos_embedding(tensor)
+print(K.eval(embeddings))
